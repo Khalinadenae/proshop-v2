939f092bf2d589a08c3b7b8cacd4459d6ee88c79
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2023-09-28 20:07:33 -0400

    about section added

--- frontend/src/screens/Newhomepage.js
+++ frontend/src/screens/Newhomepage.js
@@ -393,7 +393,17 @@ const Newhomepage = () => {
         </div>
     </section>
     
-
+    <div class="about-container">
+  <img src="assets/images/demo/look-4.jpg" alt="Snow" style={{width:"100vw", height:"50vh", margin:"30px 0", opacity:".8"}}/>
+  <div class="centered">
+    <h4 style={{color:"white"}}>
+      Who We Are
+    </h4>
+    <p>
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+  </div>
+</div>
       
       
   <div className="swiper-container-wrapper">
